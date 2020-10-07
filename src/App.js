9affe71e0024f9616a1c4a900a08225b37c84a4a
GarvinH <garvinh2001@hotmail.com>
2020-10-07 16:29:36 -0400

    Add modal for showing notes and topics

--- src/App.js
+++ src/App.js
@@ -5,40 +5,51 @@ import NoteEditor from "./components/NoteEditor/NoteEditor.js";
 import { MediaHandler } from "./components/MediaHandler/MediaHandler";
 
 class App extends React.Component {
-  state = {
-    showNoteSelctor: true,
-    showNote: false,
-  };
+  constructor(props) {
+    super(props);
+    const notes = Notes.getNotes();
+    console.log(notes);
+    this.state = {
+      notes: notes,
+      showNoteSelctor: true,
+      showNote: false,
+    };
+  }
 
   setShowNoteSelector = (showNoteSelctor) =>
     this.setState({ showNoteSelctor: showNoteSelctor });
 
-  setShowNote = (showNote) => this.setState({showNote: showNote});
+  setShowNote = (showNote) => this.setState({ showNote: showNote });
 
   render() {
-    console.log(Notes.getNotes())
-    Notes.setNotes([
-      { topic: "topic1", notes: [] },
-      { topic: "topic2", notes: [] },
-    ])
+    const { notes, showNoteSelctor } = this.state;
     return (
-      <div style={{ textAlign: "center" }}>
-        {/* <button
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button
           style={{ position: "absolute" }}
           onClick={() => this.setShowNoteSelector(true)}
         >
           Select Topic/Note
         </button>
-        <MediaHandler mediaLocation="C:\Users\carin\Pictures\Taobao\harry-potter-note-pad-01.jpg"/> */}
         <button
-          style={{textAlign: "center"}}
-          onClick={() => this.setShowNote(true)}>Open note</button>
-        <NoteEditor show={this.state.showNote} controlShow={this.setShowNote} title= "Untitled note" info="Info" keyword="Enter keywords" media="C:\Users\carin\Pictures\Taobao\harry-potter-note-pad-01.jpg"/>
-        <NoteSelector
-          show={this.state.showNoteSelctor}
-          controlShow={this.setShowNoteSelector}
+          style={{ textAlign: "center" }}
+          onClick={() => this.setShowNote(true)}
+        >
+          Open note
+        </button>
+        <NoteEditor
+          show={this.state.showNote}
+          controlShow={this.setShowNote}
+          title="Untitled note"
+          info="Info"
+          keyword="Enter keywords"
+          media="C:\Users\carin\Pictures\Taobao\harry-potter-note-pad-01.jpg"
         />
-        
+        <NoteSelector
+          show={showNoteSelctor}
+          controlShow={this.setShowNoteSelector}
+          notes={notes}
+        />
       </div>
     );
   }
--- src/components/NoteSelector/NoteSelector.js
+++ src/components/NoteSelector/NoteSelector.js
@@ -1,5 +1,15 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
+import React, { Fragment } from "react";
+import {
+  Modal,
+  Accordion,
+  Card,
+  Button,
+  Col,
+  Row,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import _ from "lodash";
 
 export default class NoteSelector extends React.Component {
   hideModal = () => {
@@ -9,19 +19,59 @@ export default class NoteSelector extends React.Component {
   };
 
   render() {
-    const { show } = this.props;
+    const { show, notes } = this.props;
+
+    const accordion_children = _.map(notes, (topic, index) => (
+      <Card key={index}>
+        <Card.Header
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Accordion.Toggle as={Button} variant="link" eventKey={`${index}`}>
+            {topic.topic}
+          </Accordion.Toggle>
+
+          <Button variant="dark">Add Note</Button>
+        </Card.Header>
+        <Accordion.Collapse eventKey={`${index}`}>
+          <Card.Body>
+            {_.map(topic.notes, (note) => (
+              <Fragment>
+                {console.log(note)}
+                <Row>
+                  <Col>{note.title}</Col>
+                  <Col>{new Date(note.date).toDateString()}</Col>
+                </Row>
+              </Fragment>
+            ))}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    ));
 
     return (
       <Modal show={show} onHide={this.hideModal} size="lg" centered>
         <Modal.Header closeButton>
           <Modal.Title>Topic/Note Selector</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Accordion>{accordion_children}</Accordion>
+        </Modal.Body>
         <Modal.Footer>
-          <div className="mr-auto">
-            <input></input>
-            <button style={{marginLeft: "1rem"}}>Create new topic</button>
-          </div>
+          <Form className="mr-auto">
+            <Form.Row>
+              <Col>
+                <FormControl
+                  placeholder="Topic name"
+                  style={{ display: "inline-block" }}
+                />
+              </Col>
+              <Col>
+                <Button variant="dark" style={{ marginLeft: "1rem" }}>
+                  Create new topic
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
         </Modal.Footer>
       </Modal>
     );
