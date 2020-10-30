27a6f0fb3827fbb7940db3240460e658eb2fa05b
GarvinH <garvinh2001@hotmail.com>
2020-10-29 22:02:28 -0400

    Add adding notes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NoteSelector from "./components/NoteSelector/NoteSelector";
 import Notes from "./backend/Notes/notes";
 import NoteEditor from "./components/NoteEditor/NoteEditor.js";
 import { MediaHandler } from "./components/MediaHandler/MediaHandler";
+import _ from "lodash";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,6 +21,31 @@ class App extends React.Component {
     this.setState({ showNoteSelctor: showNoteSelctor });
 
   setShowNote = (showNote) => this.setState({ showNote: showNote });
+
+  addNote = (topicIndex) => {
+    const { notes } = this.state;
+    const newNotes = _.map(notes, (topic, index) => {
+      if (index !== topicIndex) {
+        return { ...topic };
+      } else {
+        return {
+          topic: topic.topic,
+          notes: [
+            ...topic.notes,
+            {
+              title: `Note ${topic.notes.length + 1}`,
+              date: new Date(),
+              info: "",
+              media: "",
+              keywords: "",
+            },
+          ],
+        };
+      }
+    });
+
+    this.setState({ notes: newNotes });
+  };
 
   render() {
     const { notes, showNoteSelctor } = this.state;
@@ -49,6 +75,7 @@ class App extends React.Component {
           show={showNoteSelctor}
           controlShow={this.setShowNoteSelector}
           notes={notes}
+          addNote={this.addNote}
         />
       </div>
     );
