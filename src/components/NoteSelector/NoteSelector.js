6e7b3bc38380720ba302cd5fc784d8f352735192
GarvinH <garvinh2001@hotmail.com>
2020-11-02 19:15:18 -0500

    Link selector with editor

--- src/components/NoteSelector/NoteSelector.js
+++ src/components/NoteSelector/NoteSelector.js
@@ -30,31 +30,40 @@ export default class NoteSelector extends React.Component {
   };
 
   render() {
-    const { show, notes, addNote } = this.props;
+    const { show, notes, addNote, setNoteEditor } = this.props;
     const { topicNameInput } = this.state;
 
-    const accordion_children = _.map(notes, (topic, index) => (
-      <Card key={index}>
+    const accordion_children = _.map(notes, (topic, topicIndex) => (
+      <Card key={topicIndex}>
         <Card.Header
           style={{ display: "flex", justifyContent: "space-between" }}
         >
-          <Accordion.Toggle as={Button} variant="link" eventKey={`${index}`}>
+          <Accordion.Toggle
+            as={Button}
+            variant="link"
+            eventKey={`${topicIndex}`}
+          >
             {topic.topic}
           </Accordion.Toggle>
 
-          <Button variant="dark" onClick={() => addNote(index)}>
+          <Button variant="dark" onClick={() => addNote(topicIndex)}>
             Add Note
           </Button>
         </Card.Header>
-        <Accordion.Collapse eventKey={`${index}`}>
+        <Accordion.Collapse eventKey={`${topicIndex}`}>
           <Card.Body>
-            {_.map(topic.notes, (note, index) => (
-              <Fragment key={index}>
+            {_.map(topic.notes, (note, noteIndex) => (
+              <Fragment key={noteIndex}>
                 <Row>
                   <Col style={{ display: "flex", alignItems: "center" }}>
                     {note.title}
                   </Col>
-                  <Button variant="dark">Edit Note</Button>
+                  <Button
+                    variant="dark"
+                    onClick={() => setNoteEditor(topicIndex, noteIndex)}
+                  >
+                    Edit Note
+                  </Button>
                   <Col>{new Date(note.date).toDateString()}</Col>
                   <Button style={{ margin: "0 1rem" }} variant="dark">
                     View Connected Notes
